63343b3f60c59153c56dfadc5ac8bbbb6045373e
Vladislav ST <adrenaline2142@gmail.com>
2022-04-13 18:55:21 +0500

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,23 +1,56 @@
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+// Склонение существительных после числительного
+const numDecline = (n, form1, form2, form3) => {
+  n = Math.abs(n) % 100;
+  const n1 = n % 10;
+  if (n > 10 && n < 20) {
+    return form3;
+  }
+  if (n1 > 1 && n1 < 5) {
+    return form2;
+  }
+  if (n1 === 1) {
+    return form1;
+  }
+  return form3;
 };
 
-const getRandomPositiveFloat = (a, b, digits = 1) => {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
-  const result = Math.random() * (upper - lower) + lower;
-  return +result.toFixed(digits);
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.width = '300px';
+  alertContainer.style.right = '50%';
+  alertContainer.style.transform = 'translateX(50%)';
+  alertContainer.style.top = '55px';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '12px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = '#ffaa99';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, 2000);
 };
 
-const getRandomArrayElement = (items) => items[getRandomPositiveInteger(0, items.length - 1)];
-
-const getRandomArray = (elements) => {
-  const features = new Array(getRandomPositiveInteger(1, elements.length));
-  for (let i = 0; i < features.length; i++) {features[i] = elements[i];}
-  return features;
+// Функция debounce - устранения дребезга
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
 };
 
-export {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement, getRandomArray,};
+export {
+  numDecline,
+  isEscapeKey,
+  showAlert,
+  debounce,
+};
