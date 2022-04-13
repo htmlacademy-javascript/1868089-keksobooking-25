4dbd795e789f1359ffcd5eb8e2c3b0561dab3ec2
Vladislav ST <adrenaline2142@gmail.com>
2022-04-13 20:07:59 +0500

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,24 +1,23 @@
+const Messages = {
+  GET_NO_ADVERT: 'Не удалось получить данные с сервера :(',
+  FIND_NO_ADVERT: 'Не удалось найти подходящие объявления',
+};
+
 const ServerUrl = {
   GET_URL: 'https://25.javascript.pages.academy/keksobooking/data',
   POST_URL: 'https://25.javascript.pages.academy/keksobooking',
 };
 
-
-const Messages = {
-  GET_NO_ADS: 'Не удалось получить данные с сервера :(',
-  FIND_NO_ADS: 'Не удалось найти подходящие объявления',
-};
-
 const DEFAULT_VALUE = 'any';
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-const COUNT_OF_ADS = 10;
+const COUNT_OF_ADVERTS = 10;
 const MAP_ZOOM = 13;
 const RERENDER_DELAY = 500;
 const NUMBER_AFTER_POINT = 5;
 const MAX_PRICE_FOR_NIGHT = 100000;
 const PRICE_STEP = 100;
 
-const AdsTypes = {
+const AdvertsType = {
   PALACE: 'palace',
   FLAT: 'flat',
   HOUSE: 'house',
@@ -27,20 +26,20 @@ const AdsTypes = {
   ANY: 'any',
 };
 
-const adTypesToReadable = {
-  [AdsTypes.PALACE]: 'Дворец',
-  [AdsTypes.FLAT]: 'Квартира',
-  [AdsTypes.HOUSE]: 'Дом',
-  [AdsTypes.BUNGALOW]: 'Бунгало',
-  [AdsTypes.HOTEL]: 'Отель',
+const advertTypeEnToRu = {
+  [AdvertsType.PALACE]: 'Дворец',
+  [AdvertsType.FLAT]: 'Квартира',
+  [AdvertsType.HOUSE]: 'Дом',
+  [AdvertsType.BUNGALOW]: 'Бунгало',
+  [AdvertsType.HOTEL]: 'Отель',
 };
 
 const adTypesToPrice = {
-  [AdsTypes.PALACE]: 10000,
-  [AdsTypes.FLAT]: 1000,
-  [AdsTypes.HOUSE]: 5000,
-  [AdsTypes.BUNGALOW]: 0,
-  [AdsTypes.HOTEL]: 3000,
+  [AdvertsType.PALACE]: 10000,
+  [AdvertsType.FLAT]: 1000,
+  [AdvertsType.HOUSE]: 5000,
+  [AdvertsType.BUNGALOW]: 0,
+  [AdvertsType.HOTEL]: 3000,
 };
 
 const MAIN_LOCATION = {
@@ -77,11 +76,11 @@ const ROOMS_GUESTS_OPTIONS = {
 export {
   ServerUrl,
   Messages,
-  AdsTypes,
-  adTypesToReadable,
+  AdvertsType,
+  advertTypeEnToRu,
   adTypesToPrice,
   FILE_TYPES,
-  COUNT_OF_ADS,
+  COUNT_OF_ADVERTS,
   MAP_ZOOM,
   RERENDER_DELAY,
   MAIN_LOCATION,
